e91face7d3cade77725621de4e511ee00419f26e
jmtes <juliennetesoro@gmail.com>
2021-06-20 13:57:07 -0700

    Add Cherry Peel lyrics

--- src/lyrics.js
+++ src/lyrics.js
@@ -1,1 +1,453 @@
-module.exports = [];
+module.exports = [
+  {
+    lyric:
+      'when you come around, everything else disappears\nmy ears disappear',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric: 'when you come around, everything else vanishes',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      'the crowds slowly fade when you come around\neverything evaporates when you come around',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      "on the news at night, they talk about the disappearances\nthey interview the people who were witnesses\nmost are shocked, some don't care",
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      'when you come around, everything else disappears\nmy mouth disappears',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      'the birds have no heads when you come around\neverything loses its legs when you come around',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      'the birds have no heads when you come around\neverything loses its legs when you come around',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      "on the news at night, they talk about the abnormalities\nthey interview the people who are authorities\nmost are intrigued, some don't care",
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      'when you come around, when you are near, i feel so far away from the person i am',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric: "alone I feel like i'm jumping on a trampoline",
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      'when you come around, everything else disappears\nmy eyes disappear',
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      "the sunlight swims away when you come around\nand even darkness won't stay when you come around",
+    spotifyUrl:
+      'https://open.spotify.com/track/2lp5nigkNqpP15kHS9Sizd?si=0048d96c45124e94',
+  },
+  {
+    lyric:
+      "i'm so glad i have you to think of, to channel all my loving energy into\neven though my feelings aren't returned and in time i won't think about you this way\nthat still doesn't stop me if just for now\nwishing we were each other's baby, baby",
+    spotifyUrl:
+      'https://open.spotify.com/track/6SDyfDhgoDl5VIjA05RbVb?si=fcb2ec23106941d8',
+  },
+  {
+    lyric:
+      "i feel these emotions for you, still i know there's no way that you're the person i imagine you to be",
+    spotifyUrl:
+      'https://open.spotify.com/track/6SDyfDhgoDl5VIjA05RbVb?si=fcb2ec23106941d8',
+  },
+  {
+    lyric:
+      "i don't know if it was chance or if this happened by fate\ni don't expect you to feel the same but if you did, well that would be great",
+    spotifyUrl:
+      'https://open.spotify.com/track/6SDyfDhgoDl5VIjA05RbVb?si=fcb2ec23106941d8',
+  },
+  {
+    lyric:
+      "minutes can make memories that last a lifetime\ni distort my minutes of beauty into something that they're not",
+    spotifyUrl:
+      'https://open.spotify.com/track/6SDyfDhgoDl5VIjA05RbVb?si=fcb2ec23106941d8',
+  },
+  {
+    lyric:
+      "i can't stop your memory from breaking my heart\ndevils were laughing so loud, i couldn't hear the radio",
+    spotifyUrl:
+      'https://open.spotify.com/track/7CdtMkXZmH1Z1gcnQiMxLs?si=37262fd697ec4820',
+  },
+  {
+    lyric:
+      'in the dark bags become sneaking devils waiting to pounce and display impressive claws\ni might wink at them before they land',
+    spotifyUrl:
+      'https://open.spotify.com/track/7CdtMkXZmH1Z1gcnQiMxLs?si=37262fd697ec4820',
+  },
+  {
+    lyric:
+      "i can hear my heartbeat across the room behind the closet door when i'm laying in my bed in the dark\ni can't gather all the love i need when i need it",
+    spotifyUrl:
+      'https://open.spotify.com/track/7CdtMkXZmH1Z1gcnQiMxLs?si=37262fd697ec4820',
+  },
+  {
+    lyric:
+      "i'm not mad, i understand that that's the price\nand all is forgiven in the end if your heart was in the right place\nif you lived an honest life, if you tried",
+    spotifyUrl:
+      'https://open.spotify.com/track/7CdtMkXZmH1Z1gcnQiMxLs?si=37262fd697ec4820',
+  },
+  {
+    lyric:
+      "larry, when you're loved like you are, you won't ever feel alone, you won't be left alone",
+    spotifyUrl:
+      'https://open.spotify.com/track/1eX7OXOSXGTpG0BxbUHR5m?si=2b00cc0ea6a64159',
+  },
+  {
+    lyric:
+      "larry, when you're loved like you are, people think of you and smile\nthey think of you and they're glad",
+    spotifyUrl:
+      'https://open.spotify.com/track/1eX7OXOSXGTpG0BxbUHR5m?si=2b00cc0ea6a64159',
+  },
+  {
+    lyric: 'we will be with you\nthat means you will be with us, too',
+    spotifyUrl:
+      'https://open.spotify.com/track/1eX7OXOSXGTpG0BxbUHR5m?si=2b00cc0ea6a64159',
+  },
+  {
+    lyric: "larry, when you're loved like you are, everything falls into place",
+    spotifyUrl:
+      'https://open.spotify.com/track/1eX7OXOSXGTpG0BxbUHR5m?si=2b00cc0ea6a64159',
+  },
+  {
+    lyric:
+      "how will I ever know you enough to love you if you're hiding who you are?",
+    spotifyUrl:
+      'https://open.spotify.com/track/5g7CeXmXjqDSho3OwdIxny?si=86c82ad943a34fc0',
+  },
+  {
+    lyric:
+      'who are you hiding you from across the table with a penny in each eye?',
+    spotifyUrl:
+      'https://open.spotify.com/track/5g7CeXmXjqDSho3OwdIxny?si=86c82ad943a34fc0',
+  },
+  {
+    lyric:
+      "i'd like to marry all of my close friends and live in a big house together by an angry sea",
+    spotifyUrl:
+      'https://open.spotify.com/track/5g7CeXmXjqDSho3OwdIxny?si=86c82ad943a34fc0',
+  },
+  {
+    lyric:
+      "am i the devil's marbles? don't move on without me, who will be watching my body when i sleep? who will i believe in?",
+    spotifyUrl:
+      'https://open.spotify.com/track/5g7CeXmXjqDSho3OwdIxny?si=86c82ad943a34fc0',
+  },
+  {
+    lyric: "how am i supposed to let it show when i don't even know?",
+    spotifyUrl:
+      'https://open.spotify.com/track/5g7CeXmXjqDSho3OwdIxny?si=86c82ad943a34fc0',
+  },
+  {
+    lyric:
+      "i don't want to be the one who's coming out first\ni'd really like to, but i'm just too shy",
+    spotifyUrl:
+      'https://open.spotify.com/track/5g7CeXmXjqDSho3OwdIxny?si=86c82ad943a34fc0',
+  },
+  {
+    lyric:
+      "it's so easy to laugh to myself and pretend that i could love you, but i can't",
+    spotifyUrl:
+      'https://open.spotify.com/track/5g7CeXmXjqDSho3OwdIxny?si=86c82ad943a34fc0',
+  },
+  {
+    lyric:
+      'you remind me of a snowflake that falls while comets hit the earth,\ndestroying buildings and trees, but you are so light in the air you never disrupt a thing',
+    spotifyUrl:
+      'https://open.spotify.com/track/6DK22DFnfsxEUB4r3hShqA?si=38bac69207674339',
+  },
+  {
+    lyric: 'orenda, if you were here, i could walk through you',
+    spotifyUrl:
+      'https://open.spotify.com/track/6DK22DFnfsxEUB4r3hShqA?si=38bac69207674339',
+  },
+  {
+    lyric:
+      "in my bed at night i dream you're a ghost that only cats can see\nsome nights you talk to me, some nights you sing me to sleep while moonlight showers the room",
+    spotifyUrl:
+      'https://open.spotify.com/track/6DK22DFnfsxEUB4r3hShqA?si=38bac69207674339',
+  },
+  {
+    lyric:
+      "in dreams i dance with you\nwe dance the best in the kitchen while murderers and rapists surround the house\nwe don't care because our house is made of feathers\nthey wouldn't dare",
+    spotifyUrl:
+      'https://open.spotify.com/track/6DK22DFnfsxEUB4r3hShqA?si=38bac69207674339',
+  },
+  {
+    lyric: 'in dreams i dance with you, though our bodies are made out of wood',
+    spotifyUrl:
+      'https://open.spotify.com/track/6DK22DFnfsxEUB4r3hShqA?si=38bac69207674339',
+  },
+  {
+    lyric:
+      "sleeping in the beetle bug with a hundred pounds of air in my heart\ndon't think that i'm able to sympathize, i'm happier to see it gone",
+    spotifyUrl:
+      'https://open.spotify.com/track/0TZTHLS6IDfOkq4OMDEhAZ?si=3e144ae456d64f2e',
+  },
+  {
+    lyric:
+      'floating above your house like a penguin dropping cherries from my mouth, tapping the walnuts and the shadows out of a dreaming pair of brown eyed ghosts',
+    spotifyUrl:
+      'https://open.spotify.com/track/0TZTHLS6IDfOkq4OMDEhAZ?si=3e144ae456d64f2e',
+  },
+  {
+    lyric:
+      "in each of your eyes, i saw it's spring\nwhere every mouth wakes up to a smile and a yawn,\ngrass is long and laughs when the wind jumps through it",
+    spotifyUrl:
+      'https://open.spotify.com/track/0TZTHLS6IDfOkq4OMDEhAZ?si=3e144ae456d64f2e',
+  },
+  {
+    lyric:
+      "it must have started with that stick in the mud, that there's where clouds are born\nclouds can't stay where they are born, winds push them so far from home",
+    spotifyUrl:
+      'https://open.spotify.com/track/0TZTHLS6IDfOkq4OMDEhAZ?si=3e144ae456d64f2e',
+  },
+  {
+    lyric:
+      'the sound of your laughter tiptoeing across the floor makes the deepest of red umbrellas able to inflate my smile',
+    spotifyUrl:
+      'https://open.spotify.com/track/0TZTHLS6IDfOkq4OMDEhAZ?si=3e144ae456d64f2e',
+  },
+  {
+    lyric:
+      "tim, wish you were born a girl\nwish you were born a girl so i could've been your boyfriend\ni know it's not possible now, i just never met a girl i like half as much as you",
+    spotifyUrl:
+      'https://open.spotify.com/track/7omVcCHKJOQTDPVm6QoDnr?si=2f8f117098cc4076',
+  },
+  {
+    lyric:
+      'we could lay around in bed, stay there all day or at least until the afternoon\nand i could make you spaghetti with tomato sauce, with just a touch of oregano and a parsley stem',
+    spotifyUrl:
+      'https://open.spotify.com/track/7omVcCHKJOQTDPVm6QoDnr?si=2f8f117098cc4076',
+  },
+  {
+    lyric:
+      "then when you got sick i could take the day off work\ni could've made you chicken soup and we could watch soap operas, oh, those tv dramas!\ni could catch your cold and you could take care of me",
+    spotifyUrl:
+      'https://open.spotify.com/track/7omVcCHKJOQTDPVm6QoDnr?si=2f8f117098cc4076',
+  },
+  {
+    lyric:
+      "if i could've met you at school or met you at work, it would have changed everything\nthose years of losing, confusion and insecurity\nthey would have been shared, they would have been easier",
+    spotifyUrl:
+      'https://open.spotify.com/track/7omVcCHKJOQTDPVm6QoDnr?si=2f8f117098cc4076',
+  },
+  {
+    lyric:
+      "oh tim, wish you were born a girl\nwish you were born a girl so i could've been your fiancé\ni'm not saying you can't be all these things for me, but it's just not the same 'cause you're a man and so am i",
+    spotifyUrl:
+      'https://open.spotify.com/track/7omVcCHKJOQTDPVm6QoDnr?si=2f8f117098cc4076',
+  },
+  {
+    lyric:
+      "montreal is where i began to feel inside the gray sadness of winter, when you told me that this isn't it and i drove away",
+    spotifyUrl:
+      'https://open.spotify.com/track/1OYcbfu20NKadYhUKhxbmq?si=e0abe35b87e744fe',
+  },
+  {
+    lyric:
+      'walking to the statue in the park through snow drifts up over our knees and every street sign written in french\nwe sat by the statue, you looked in my eyes, then said, "i\'m so sorry"',
+    spotifyUrl:
+      'https://open.spotify.com/track/1OYcbfu20NKadYhUKhxbmq?si=e0abe35b87e744fe',
+  },
+  {
+    lyric:
+      "later, laying on your bed wondering what's going wrong\nevery time i'd ask you'd start crying and whisper, \"i don't know why, i only know what i feel, what a voice says to me\"",
+    spotifyUrl:
+      'https://open.spotify.com/track/1OYcbfu20NKadYhUKhxbmq?si=e0abe35b87e744fe',
+  },
+  {
+    lyric: 'i may be here now, but i never left montreal',
+    spotifyUrl:
+      'https://open.spotify.com/track/1OYcbfu20NKadYhUKhxbmq?si=e0abe35b87e744fe',
+  },
+  {
+    lyric:
+      "i'm so excited, i just can't wait for you to come\ni've been living alone and lonely far too long",
+    spotifyUrl:
+      'https://open.spotify.com/track/7JLav98rPrstSZg34ugz7D?si=575c3fda33694729',
+  },
+  {
+    lyric:
+      "when i see your face and the love that's in your eyes you know i just can't place this feeling",
+    spotifyUrl:
+      'https://open.spotify.com/track/7JLav98rPrstSZg34ugz7D?si=575c3fda33694729',
+  },
+  {
+    lyric:
+      "i'm so excited all my sad days will soon be over\ni know you can do it because you did it before and i'm happy when i'm with you",
+    spotifyUrl:
+      'https://open.spotify.com/track/7JLav98rPrstSZg34ugz7D?si=575c3fda33694729',
+  },
+  {
+    lyric:
+      "i guess it's a lot to ask of you to change everything that's bad, you know i have to laugh at this feeling",
+    spotifyUrl:
+      'https://open.spotify.com/track/7JLav98rPrstSZg34ugz7D?si=575c3fda33694729',
+  },
+  {
+    lyric:
+      "there's one thing i hesitate to add: what if it turns out bad? we'll be living together, will we be spending too much time together?",
+    spotifyUrl:
+      'https://open.spotify.com/track/7JLav98rPrstSZg34ugz7D?si=575c3fda33694729',
+  },
+  {
+    lyric:
+      "i'm gonna find out just how close we really are and if having you around is better than being alone",
+    spotifyUrl:
+      'https://open.spotify.com/track/7JLav98rPrstSZg34ugz7D?si=575c3fda33694729',
+  },
+  {
+    lyric:
+      "i want to write something beautiful, something so beautiful that i just can't sing it without getting that incredible feeling\nthe one that just overwhelms my senses because it's the only thing i have, it's the only thing that makes my life worthwhile",
+    spotifyUrl:
+      'https://open.spotify.com/track/1Cc6sjFKaEHt2fIcZvb1Bi?si=b43167479a47487a',
+  },
+  {
+    lyric:
+      "i know there aren't too many people who allow themselves to say what they feel, so i was watching your eyes in case they just might say something",
+    spotifyUrl:
+      'https://open.spotify.com/track/1Cc6sjFKaEHt2fIcZvb1Bi?si=b43167479a47487a',
+  },
+  {
+    lyric:
+      "i want to think someone's beautiful, that they're so beautiful it's a thrill to be near them",
+    spotifyUrl:
+      'https://open.spotify.com/track/1Cc6sjFKaEHt2fIcZvb1Bi?si=b43167479a47487a',
+  },
+  {
+    lyric:
+      "fill my stomach with butterflies, have me floating on air, bring something out of me i didn't know was there",
+    spotifyUrl:
+      'https://open.spotify.com/track/1Cc6sjFKaEHt2fIcZvb1Bi?si=b43167479a47487a',
+  },
+  {
+    lyric: "it's such a torturous thing that i am not excited by anyone",
+    spotifyUrl:
+      'https://open.spotify.com/track/1Cc6sjFKaEHt2fIcZvb1Bi?si=b43167479a47487a',
+  },
+  {
+    lyric:
+      "the summer's good for tulips, though pansies disagree\nthey find the heat most distasteful and humidity far too grim to stand up tall and bargain with the bees\nthey prefer to droop and mope and wait for autumn's breeze",
+    spotifyUrl:
+      'https://open.spotify.com/track/6LYTsXEjmriPPQmBefRJRa?si=0190c70b64834bf5',
+  },
+  {
+    lyric:
+      "the autumn's good for pumpkins, though apples don't approve\nthe trees that they've been living on now force them to move\nand rudely lets them to fall, and sends them quick to their demise\nwithout so much a bon voyage or even a goodbye",
+    spotifyUrl:
+      'https://open.spotify.com/track/6LYTsXEjmriPPQmBefRJRa?si=0190c70b64834bf5',
+  },
+  {
+    lyric:
+      "the winter's good for penguins, though brown bears must object\nwhen talk comes to the joys of winter, they must interject, \"hibernating in the snow just isn't where it's at 'cause sleeping makes you skinny and we bears like to be fat\"",
+    spotifyUrl:
+      'https://open.spotify.com/track/6LYTsXEjmriPPQmBefRJRa?si=0190c70b64834bf5',
+  },
+  {
+    lyric:
+      "the springtime is the season where everyone's a friend, loneliness and desperation both come to an end",
+    spotifyUrl:
+      'https://open.spotify.com/track/6LYTsXEjmriPPQmBefRJRa?si=0190c70b64834bf5',
+  },
+  {
+    lyric:
+      "no matter how you died through winter, in spring you're born again\nyour life might not be going good but spring helps you to pretend",
+    spotifyUrl:
+      'https://open.spotify.com/track/6LYTsXEjmriPPQmBefRJRa?si=0190c70b64834bf5',
+  },
+  {
+    lyric:
+      'everything is falling apart, yes\ni am watching me watch birds watch people',
+    spotifyUrl:
+      'https://open.spotify.com/track/5v7avtXAQJAvWyP21ah1Am?si=b959af3867b04fc0',
+  },
+  {
+    lyric:
+      "i wear my bowtie to bed 'cause i'm waiting for the gorgeous young man hiding in the spaces",
+    spotifyUrl:
+      'https://open.spotify.com/track/5v7avtXAQJAvWyP21ah1Am?si=b959af3867b04fc0',
+  },
+  {
+    lyric:
+      'if we are only waiting here to die, at least let me put my arms around you',
+    spotifyUrl:
+      'https://open.spotify.com/track/5v7avtXAQJAvWyP21ah1Am?si=b959af3867b04fc0',
+  },
+  {
+    lyric:
+      'why try to keep it from myself when it feels so good to sing about love?',
+    spotifyUrl:
+      'https://open.spotify.com/track/5v7avtXAQJAvWyP21ah1Am?si=b959af3867b04fc0',
+  },
+  {
+    lyric: 'everything is falling apart, yes i am',
+    spotifyUrl:
+      'https://open.spotify.com/track/5v7avtXAQJAvWyP21ah1Am?si=b959af3867b04fc0',
+  },
+  {
+    lyric: "i can see that you're rubbing off on everyone you know",
+    spotifyUrl:
+      'https://open.spotify.com/track/0PXrJ1nGivMOMCIbzhYPUJ?si=776028287f9c414f',
+  },
+  {
+    lyric:
+      "you're rubbing off on me, i can feel your incredible glow\ni can hear your happy voice in other people's voices\ni can hear your beautiful voice sometimes when i speak",
+    spotifyUrl:
+      'https://open.spotify.com/track/0PXrJ1nGivMOMCIbzhYPUJ?si=776028287f9c414f',
+  },
+  {
+    lyric:
+      "you've got a special gift, do you see how you're changing the world just by hanging around?",
+    spotifyUrl:
+      'https://open.spotify.com/track/0PXrJ1nGivMOMCIbzhYPUJ?si=776028287f9c414f',
+  },
+  {
+    lyric:
+      'i can see how you sweetened up both of your grumpy roommates, now they even say "hello" when we pass on the street',
+    spotifyUrl:
+      'https://open.spotify.com/track/0PXrJ1nGivMOMCIbzhYPUJ?si=776028287f9c414f',
+  },
+  {
+    lyric:
+      'i find joy in simple things ever since i met you, i never get the chance just to tell you how out of sight you are',
+    spotifyUrl:
+      'https://open.spotify.com/track/0PXrJ1nGivMOMCIbzhYPUJ?si=776028287f9c414f',
+  },
+  {
+    lyric:
+      'i find joy in simple things ever since i met you, i never get the chance just to tell you how out of sight you are',
+    spotifyUrl:
+      'https://open.spotify.com/track/0PXrJ1nGivMOMCIbzhYPUJ?si=776028287f9c414f',
+  },
+];
